c5abaf7504750823e3905be9fd2b8553343b58f8
Peder Smith <smith.peder@gmail.com>
2019-03-06 21:53:58 +0100

    Remove junk

--- selectData.sql
+++
@@ -1,9 +0,0 @@
-USE MD;
-
-select MovieId, Title, ProductionYear, DirectorID from Movie;
-
-select Name, Birthyear from Actor where Birthyear > 1960;
-
-select Name, Birthyear from Actor where 1970 <= Birthyear and Birthyear < 1980 order by Name; 
-
-select Title, Role from Actor as A join ActorInMovie as AIM on A.ActorID = AIM.ActorID join Movie as M on M.MovieID = AIM.MovieID where A.Name = "Morgan Freeman";
---
+++ test_data.sql
@@ -0,0 +1,8 @@
+use wd;
+
+insert into exercise (id, name, description) values (1,'Jogging','Running in moderate tempo for an extended period of time');
+insert into exercise (id, name, description) values (2,'Running','Running in high tempo for a period of time');
+insert into exercise (id, name, description) values (3,'Bench press','Lifting a bar while on a bench');
+insert into exercise (id, name, description) values (4,'Pull up','Pulling up your own body from a bar');
+insert into exercise (id, name, description) values (5,'Push up','Pushing up your own body using your arms');
+insert into exercise (id, name, description) values (6,'Sit up','Using your abs to lift your tourso');
