44bc40410876e932637c9dd3f9b696264e8e69dc
Peder Smith <smith.peder@gmail.com>
2019-03-08 21:55:08 +0100

    Add EquipmentController for equipment actions

---
+++ src/EquipmentController.java
@@ -0,0 +1,45 @@
+import java.sql.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class EquipmentController {
+  Statement stmt = null;
+  Connection CON = DatabaseController.CON;
+
+  public void start() {
+    int userInput;
+    Boolean next = true;
+    Scanner in = new Scanner(System.in);
+
+    while (next) {
+      JDBC.OUTPUT.equipmentMenu();
+      System.out.print("User choise: ");
+      userInput = Integer.valueOf(in.next());
+
+      switch(userInput) {
+        case 0:
+          next = false;
+          break;
+        case 1:
+          getAll();
+          break;
+        default:
+          System.out.println("DEFAULT");
+      }
+    }
+
+  }
+
+  private void getAll() {
+    try {
+      stmt = CON.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM equipment;");
+      ResultSetMetaData rsmd = rs.getMetaData();
+      JDBC.OUTPUT.printColumns(rs, rsmd);
+    } catch(SQLException sql) {
+      JDBC.OUTPUT.red(sql.toString());
+    }
+  }
+}
