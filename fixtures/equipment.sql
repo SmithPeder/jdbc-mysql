5a12cc2decf17ea1e6d3027b91c4ac147a7af6b9
Peder Smith <smith.peder@gmail.com>
2019-03-09 18:01:11 +0100

    Revert fixture changes
    If the fixtures use auto_increment we can add a lot of them after
    each other, so they stack. This was not intentional behavior.

--- fixtures/equipment.sql
+++ fixtures/equipment.sql
@@ -1,10 +1,10 @@
 use wd;
 
-insert ignore into equipment(name, description)
-	values('Bench', 'A flat board with legs');
-insert ignore into equipment(name, description)
-	values('Crossbar', 'A bar that you can hang in');
-insert ignore into equipment(name, description)
-	values('Weights', 'A pair of heavy things');
-insert ignore into equipment(name, description)
-	values('Barbell', 'A bar you can attach weights to');
+insert ignore into equipment(id, name, description)
+	values(1, 'Bench', 'A flat board with legs');
+insert ignore into equipment(id, name, description)
+	values(2, 'Crossbar', 'A bar that you can hang in');
+insert ignore into equipment(id, name, description)
+	values(3, 'Weights', 'A pair of heavy things');
+insert ignore into equipment(id, name, description)
+	values(4, 'Barbell', 'A bar you can attach weights to');
